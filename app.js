a0e73e88c4a48a35dca82fde68a34dc6b7acaf29
HarshavardhanReddy-Nusum <harshavardhanreddynusum@gmail.com>
2025-06-29 19:15:31 +0530

    added new files

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const Listing = require('./models/listing.js');
 const path = require("path");
 const methodOverride = require('method-override');
+const ejsMate = require("ejs-mate");
 
 main()
     .then((res) => { console.log("Connection successfull to wanderlust"); })
@@ -17,6 +18,8 @@ app.set("views",path.join(__dirname,"views"));
 app.set("view engine","ejs");
 app.use(express.urlencoded({extended: true}));
 app.use(methodOverride("_method"));
+app.engine("ejs",ejsMate);
+app.use(express.static(path.join(__dirname,"public")));
 
 app.get("/", (req, res) => {
     res.send("This is the root.");
